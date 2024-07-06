6a9ee1234e685326ea15c6ae6cc43e28d3783127
tushcmd <muturidavid854@gmail.com>
2024-07-06 22:31:25 +0300

    feat: drizzle config

---
+++ .history/drizzle.config_20240706221540.ts
@@ -0,0 +1,10 @@
+
+import { defineConfig } from "drizzle-kit";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is missing");
+  }
+  
